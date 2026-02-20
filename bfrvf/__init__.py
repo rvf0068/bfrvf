7a1ffc8953ea06dbaf05f1d3ec89e874c9d71cdd
Rafael Villarroel <rvf0068@gmail.com>
2026-02-20 00:06:48 +0000

    Implement Hamiltonian cycle and graph coloring algorithms

--- bfrvf/__init__.py
+++ bfrvf/__init__.py
@@ -1,2 +1,42 @@
-def hello():
-    return "Hello, world!"
+import itertools
+import networkx as nx
+
+def is_hamiltonian_cycle(graph, cycle):
+    """Checks if cycle is a hamiltonian cycle in graph.
+    graph is a Networkx graph, and cycle is a list of vertices"""
+    n = len(set(cycle))
+    if n != graph.order():
+        return False
+    for i in range(n-1):
+        if not graph.has_edge(cycle[i], cycle[i+1]):
+            return False
+    if not graph.has_edge(cycle[n-1], cycle[0]):
+        return False
+    return True
+
+def is_hamiltonian(graph):
+    if not nx.is_connected(graph):
+        return False
+    vertices = list(graph.nodes())
+    if len(vertices) < 3:
+        return False
+    perms = itertools.permutations(vertices)
+    for perm in perms:
+        if is_hamiltonian_cycle(graph, perm):
+            return perm
+    return False
+
+def is_proper_coloring(graph, coloring):
+    for edge in graph.edges():
+        if coloring[edge[0]] == coloring[edge[1]]:
+            return False
+    return True
+
+def is_3_colorable(graph):
+    n = graph.order()
+    colorings = itertools.product([0, 1, 2], repeat = n)
+    for coloring in colorings:
+        if is_proper_coloring(graph, coloring):
+            return coloring
+    return False        
+
